20d23fdb1b0df9f365fd3ec615eb50021fa7e2a9
Letícia Aguiar <letsaguiar@yandex.com>
2024-11-07 19:49:08 -0300

    feat: create map

--- src/screen_render.c
+++ src/screen_render.c
@@ -1,6 +1,6 @@
 #include "fdf.h"
 
-void    screen_render(t_screen *screen)
+void    screen_render_iter(void *params, int x, int y, int z)
 {
     size_t map_height = TILE_SIZE * 2;
     size_t screen_height = SCREEN_SIZE;
@@ -10,10 +10,11 @@ void    screen_render(t_screen *screen)
     size_t screen_width = SCREEN_SIZE;
     size_t offset_x = (screen_width - map_width) / 2;
 
-    t_point p1 = {0, 0, 0};
-    t_point p2 = {0, 20, 0};
-    t_point p3 = {20, 0, 0};
-    image_draw_square(screen->image, p1, offset_x, offset_y, 0x00FF00);
-    image_draw_square(screen->image, p2, offset_x, offset_y, 0x00FF00);
-    image_draw_square(screen->image, p3, offset_x, offset_y, 0x00FF00);
+    t_point point = {x * TILE_SIZE, y * TILE_SIZE, z};
+    image_draw_square((t_image *)params, point, offset_x, offset_y, 0x00FF00);
+}
+
+void    screen_render(t_screen *screen)
+{
+    map_iter_row(screen->map, screen_render_iter, screen->image);
 }
